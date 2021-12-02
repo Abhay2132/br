a91e05291b96f664b35820c93ab084f2bc871f11
abhay2132 <abhaybro768@gmail.com>
2021-12-02 23:44:46 +0530

    Auto Push

---
+++ src/static/views/anime.js
@@ -0,0 +1,19 @@
+function pop ( tag , tt = 200){
+    var init_trans = tag.style.transition;
+    tag.style.transition = "0.1s";
+    tag.style.transform = "scale(0.9)";
+    setTimeout(() => {
+        tag.style.transform = "scale(1)";
+        tag.style.transition = init_trans;
+    }, tt)
+}
+
+function pressed ( tag , tt = 200){
+    var init_trans = tag.style.transition;
+    tag.style.transition = "0.1s";
+    tag.style.transform = "translateY(2px)";
+    setTimeout(() => {
+        tag.style.transform = "translateY(0px)";
+        tag.style.transition = init_trans;
+    }, tt)
+}
