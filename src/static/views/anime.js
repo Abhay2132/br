1ac581c4a7231190f152fc7b1c0b86efd0286b48
abhay2132 <abhaybro768@gmail.com>
2021-12-04 12:56:05 +0530

    button click anime with box-shadow

--- src/static/views/anime.js
+++ src/static/views/anime.js
@@ -8,12 +8,12 @@ function pop ( tag , tt = 200){
     }, tt)
 }
 
-function pressed ( tag , tt = 200){
-    var init_trans = tag.style.transition;
-    tag.style.transition = "0.1s";
-    tag.style.transform = "translateY(2px)";
+function pressed ( tag , bs = false){
+    tag.style.transition = "0.2s";
+    tag.style.transform = "translateY(3px)";
+    if(bs) tag.style.boxShadow = "0px 1px 1px #666"
     setTimeout(() => {
         tag.style.transform = "translateY(0px)";
-        tag.style.transition = init_trans;
-    }, tt)
+    	if(bs) tag.style.boxShadow = "0px 4px 1px #666"
+    }, 200)
 }
