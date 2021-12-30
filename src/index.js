51769097705f74913beafc3b276386dbcabcd22e
abhay2132 <abhaybro768@gmail.com>
2021-12-30 12:46:57 +0530

    added clusters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+module.exports = () => {
 const exp = require("express"),
     app = exp(),
     rh = require("./mods/routeHander"),
@@ -19,3 +20,6 @@ app.locals.port = process.env.PORT || 5000;
 rh(app)
 
 app.listen(app.locals.port, ()=> log("Server Started at port :", app.locals.port))
+
+}
+
