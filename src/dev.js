7d866b1f18ef4f9162e76670c4e0ce592e632afe
abhay2132 <abhaybro768@gmail.com>
2022-01-04 19:25:17 +0530

    hwile

--- src/dev.js
+++ src/dev.js
@@ -1,28 +1,51 @@
 const pupp = require("puppeteer");
 
-(async function (){
-    const browser = await pupp.launch({args : ["--no-sandbox"],
-        headless : false
+console.clear();
+
+async function dev() {
+    const browser = await pupp.launch({
+        args: ["--no-sandbox"],
+        headless: false,
+        defaultViewport: null
     })
     var page = await browser.newPage();
     let url = "https://animixplay.to/v1/mob-psycho-100";
 
-    let pages = await browser.pages();
-    let frames = await page.frames();
-    console.log("pages : %i | frames : %i", pages.length, frames.length)
-
-    await page.goto(url, {waitUntil: "domcontentloaded", timeout: 0})
+    await page.goto(url, { waitUntil: "domcontentloaded", timeout: 0 })
     await page.click("i.dlbutton.glyphicon.glyphicon-download-alt")
-    // let href = await page.evaluate(new Function(`
-    //     return window.location.href`)) || ""
-    pages = await browser.pages();
-    frames = await page.frames();
 
-    console.log("pages : %i | frames : %i", pages.length, frames.length)
+    while (true) {
+        let npages = (await browser.pages()).length;
+        if (npages === 3) break;
+        await (new Promise(res => setTimeout(res, 400)))
+    } // to make sure page is opened !
+
+    let newPage = await (await browser.pages())[2]
+    console.log(newPage.url())
+    await newPage.waitForSelector("div.dowload > a")
+
+    let i720p = await newPage.$eval("div.mirror_link", (ml) => {
+        let tags = ml.children;
+        for (let i = 0; i < tags.length; i++) {
+            if (tags[i].textContent.includes("360")) return i+1;
+        }
+    })
+    console.log("720p is %ith tag ;)", i720p)
+
+    await page._client.send('Page.setDownloadBehavior', {   // Set Download Path;
+        behavior: 'allow',
+        downloadPath: __dirname 
+    });
+
+    await newPage.click(`div.mirror_link > div.dowload:nth-child(${i720p}) > a`);
+    await newPage.waitForTimeout(30000);
     browser.close();
-})()
+}
 
-const eval4download = () => (new Function(`
+dev();
+
+function eval4dlnks() {
+    return new Function(`
     var lnks = document.querySelectorAll("div.dowload > a")
     var dlnks = []
     lnks.forEach(lnk => {
@@ -30,4 +53,5 @@ const eval4download = () => (new Function(`
         if(txt.endsWith("mp4)")) dlnks.push(lnk);
     });
     return dlnks.map( a => ({href : a.getAttribute("href"), txt : a.textContent}));
-`))
+`)
+}
