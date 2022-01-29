d11916cebd9c9a0b48400d6be52941b547a6fe0e
Abhay <abhaybro768@gmail.com>
2022-01-29 14:37:36 +0530

    { args: ['--no-sandbox'] }

--- src/bin/webScrappers.js
+++ src/bin/webScrappers.js
@@ -1,7 +1,7 @@
 const pupp = require("puppeteer");
 
 const getHTM = url => new Promise( async res => {
-    const browser = await pupp.launch({headless : false})
+    const browser = await pupp.launch({ args: ['--no-sandbox'] })
     const page = await browser.newPage();
     await page.goto(url, {waitUntil : "domcontentloaded"})
     const htm = await page.content();
