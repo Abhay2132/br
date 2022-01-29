bba065dc670414d118377bd3daeeaf06a313f2e7
abhay2132 <abhaybro768@gmail.com>
2022-01-29 18:08:16 +0530

    waitUntil networkidle2

--- src/bin/webScrappers.js
+++ src/bin/webScrappers.js
@@ -3,7 +3,7 @@ const pupp = require("puppeteer");
 const getHTM = url => new Promise( async res => {
     const browser = await pupp.launch({ args: ['--no-sandbox'], headless : process.env.NODE_ENV })
     const page = await browser.newPage();
-    await page.goto(url, {waitUntil : "domcontentloaded"})
+    await page.goto(url, {waitUntil : "networkidle2"})
     const htm = await page.content();
     browser.close();
     return res(htm)
