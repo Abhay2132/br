4d1de094393589d62e872103866591a1a7724235
Abhay <abhaybro768@gmail.com>
2022-02-27 19:17:15 +0530

    fixed the path bug

--- src/bin/webScrappers.js
+++ src/bin/webScrappers.js
@@ -1,9 +1,10 @@
 const pupp = require("puppeteer-extra");
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 pupp.use(StealthPlugin())
+const mode = process.env.NODE_ENV === "production" ? { args: ['--no-sandbox'], headless : true} : { args: ['--no-sandbox'], headless : false, executablePath : "/usr/bin/google-chrome"}
 
 const getHTM = url => new Promise( async res => {
-    const browser = await pupp.launch({ args: ['--no-sandbox'], headless : !!process.env.NODE_ENV, executablePath : "/usr/bin/google-chrome"})
+    const browser = await pupp.launch(mode)
     const page = await browser.newPage();
     await page.goto(url, {waitUntil : "domcontentloaded"})
     // await page.waitForTimeout(15000)
