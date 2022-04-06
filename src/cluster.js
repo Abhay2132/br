551a6f14f6159239f8d84ddb96f25b7c2bdfc629
abhay2132 <abhaybro768@gmail.com>
2022-04-06 11:45:40 +0530

    added procfile for heroku dyno

--- src/cluster.js
+++ src/cluster.js
@@ -4,8 +4,6 @@ const numCPUs = require("os").cpus().length;
 
 if (cluster.isMaster) {
 	console.log(`Master ${process.pid} is running`);
-
-	// Fork workers.
 	for (let i = 0; i < numCPUs; i++) {
 		cluster.fork();
 	}
@@ -14,8 +12,6 @@ if (cluster.isMaster) {
 		console.log(`worker ${worker.process.pid} died`);
 	});
 } else {
-	// Workers can share any TCP connection
-	// In this case it is an HTTP server
 	app();
 	console.log(`Worker ${process.pid} started`);
 }
